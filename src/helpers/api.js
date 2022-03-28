6b43c73f6778ace00a9459bced14e09eca1d45e5
mhannani <mohamed.hanani2019@gmail.com>
2022-03-28 17:45:59 +0000

    adding new model

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -25,3 +25,42 @@ fetch("http://skyanalytics.indatacore.com:4431/check_liveness", requestOptions)
     .then(response => response.text())
     .then(result => console.log(result))
     .catch(error => console.log('error', error));
+
+
+function _base64ToArrayBuffer(base64) {
+    var binary_string =  window.atob(base64);
+    var len = binary_string.length;
+    var bytes = new Uint8Array( len );
+    for (var i = 0; i < len; i++)        {
+        bytes[i] = binary_string.charCodeAt(i);
+    }
+    return bytes.buffer;
+}
+
+function _base64ToArrayBuffer(base64) {
+    var binary_string =  window.atob(base64);
+    var len = binary_string.length;
+    var bytes = new Uint8Array( len );
+    for (var i = 0; i < len; i++)        {
+        bytes[i] = binary_string.charCodeAt(i);
+    }
+    return bytes.buffer;
+}
+_____
+compressed_image = compressed_image.replace('data:image/jpeg;base64,', '');
+var binary_data=_base64ToArrayBuffer(compressed_image)
+var myImage= new File([binary_data], "byts_document.jpg",{type:"application/octet-stream"})
+________
+var form_data = new FormData();
+form_data.append("document",myImage);
+form_data.append('institution_id',institution_id);
+form_data.append('application_id',application_id);
+form_data.append('channel_id',channel_id);
+form_data.append('service_id',service_id);
+form_data.append('sub_service_id',sub_service_id);
+form_data.append('token',token);
+form_data.append("request_data",'{"doc_type":"'+doc_type+'"}');
+$.ajax({
+    url : "http://"+serverIP+":7001/extract_info_from_document",
+    type :
+
