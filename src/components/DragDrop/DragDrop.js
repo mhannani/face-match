32270714b13a595fc463dcc2b03e8b8afe5af612
HANNANI Mohamed <mohamed.hanani2019@gmail.com>
2022-04-06 19:47:38 +0000

    face match

--- src/components/DragDrop/DragDrop.js
+++ src/components/DragDrop/DragDrop.js
@@ -2,19 +2,23 @@ import { FileUploader } from "react-drag-drop-files";
 import React from 'react'
 import './drag_drop.css'
 
-export const DragDrop = ()=>{
-    const fileTypes = ["JPEG", " PNG"];
+export const DragDrop = () => {
+    const fileTypes = ["jpeg", " png", " jpg"];
+    const handleChange = (file) => {
+        // setFile(file);
+        console.log(file)
+    };
 
     return(
         <div className={'drop_drag'}>
             <FileUploader
-                multiple={false}
+                multiple={true}
                 label = {'Drop or Drag your CIN please'}
-                // handleChange={handleChange}
+                handleChange={handleChange}
                 name="file"
                 types={fileTypes}
+                maxSize={10}
             />
         </div>
-
     )
 }
